7df0922ea422fa93f47368ceade6956fd6f7d5c6
AroonDH <aroon.delgado@unmsm.edu.pe>
2026-02-10 18:39:50 -0500

    Fix: permitir strings en queues

--- DemoQueue.cpp
+++ DemoQueue.cpp
@@ -1,11 +1,13 @@
 #include <assert.h>
 #include <fstream>
 #include <sstream>
+#include "containers/queue.h"
 #include "containers/lists.h"
 
 using namespace std;
 
 using QueueInt = CQueue< QueueTraits<T1> >;
+using QueueString = CQueue< QueueTraits<string> >;
 
 static void TestPushPop(ostream &log) {
     QueueInt q;
@@ -60,10 +62,53 @@ static void TestStreamRoundTrip(ostream &log) {
     log << "TestStreamRoundTrip: OK" << endl;
 }
 
+static void TestStringQueuePushPop(ostream &log) {
+    QueueString q;
+    q.push("first", 1);
+    q.push("second", 2);
+    q.push("third", 3);
+
+    string s1 = q.pop();
+    string s2 = q.pop();
+    string s3 = q.pop();
+
+    assert(s1 == "first");
+    assert(s2 == "second");
+    assert(s3 == "third");
+
+    log << "TestStringQueuePushPop: OK" << endl;
+}
+
+static void TestStringQueueStreamRoundTrip(ostream &log) {
+    QueueString q;
+    q.push("alpha", 10);
+    q.push("beta", 20);
+    q.push("gamma", 30);
+
+    ostringstream os;
+    os << q;
+
+    istringstream is(os.str());
+    QueueString q2;
+    is >> q2;
+
+    string s1 = q2.pop();
+    string s2 = q2.pop();
+    string s3 = q2.pop();
+
+    assert(s1 == "alpha");
+    assert(s2 == "beta");
+    assert(s3 == "gamma");
+
+    log << "TestStringQueueStreamRoundTrip: OK" << endl;
+}
+
 void DemoQueue() {
     ofstream logFile("queue_test.log");
     TestPushPop(logFile);
     TestPopEmptyThrows(logFile);
     TestStreamRoundTrip(logFile);
+    TestStringQueuePushPop(logFile);
+    TestStringQueueStreamRoundTrip(logFile);
     logFile << "DemoQueue: Done" << endl;
 }
