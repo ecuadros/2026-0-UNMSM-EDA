352210e40560ab2ba60c341c75f44e18bbde8427
rhuanto <robert.huanto@unmsm.edu.pe>
2026-02-10 13:20:06 -0500

    avance hasta ahora

--- containers/linkedlist.h
+++ containers/linkedlist.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "../general/types.h"
 #include "../util.h"
+#include <mutex> //Para poder usar mutex en concurrencia
 using namespace std;
 
 // TODO: Traits para listas enlazadas
@@ -66,47 +67,202 @@ class CLinkedList {
     // friend GeneralIterator< CLinkedList<Traits> >;
     using  Node = NodeLinkedList<Traits>;
 
-    Node *m_pRoot = nullptr;
+    //Node *m_pRoot = nullptr;
+    Node *m_pHat; //para poder hacer la lista circular y evitar casos especiales en insercion/eliminacion
     Node *m_pLast = nullptr;
     size_t m_nElements = 0;
+    std::mutex m_mutex; // Mutex para control de concurrencia
 public:
-    CLinkedList(){}
+    CLinkedList(){
+        m_pHat= new Node();
+        m_pHat->GetNextRef() = m_pHat; // Lista circular
+        m_pLast = m_pHat;
+        m_nElements = 0;
+    };
+
     // TODO: Constructor copia
+    CLinkedList(const CLinkedList &other);
+
     // TODO: Move Constructor
+    CLinkedList(CLinkedList &&other);
+
     // TODO: Destructor seguro y virtual
+    virtual ~CLinkedList();
+
     // TODO: Concurrencia (mutex)
+
     // TODO: Iterators begin() end()
+    Node* begin(){
+        return m_pHat->GetNext();
+    };
+    Node* end(){
+        return m_pHat;
+    };
+
     // TODO: Operadores de acceso []
+    value_type &operator[](size_t index);
+    const value_type &operator[](size_t index) const;
+
+    //Foreach
+    template <typename Func>
+    void Foreach(Func f){
+        Node* curr=m_pHat->GetNext();  
+        while (curr!=m_pHat){  
+            f(curr->GetValueRef());  
+            curr=curr->GetNext();  
+        }
+    }
 
     void push_back(value_type &val, ref_type ref);
     void Insert(const value_type &val, ref_type ref);
-    size_t getSize(){ return m_nElements;  }
+    size_t getSize() const { return m_nElements;  }
+
+    // TODO: Eliminar por valor, por referencia, por posición
+    void pop_back();
 private:
-    void InternalInsert(Node *&rParent, const value_type &val, ref_type ref);
+    //void InternalInsert(Node *&rParent, const value_type &val, ref_type ref);
 
     // TODO: Persistencia (write)
     friend ostream &operator<<(ostream &os, CLinkedList<Traits> &container){
         os << "CLinkedList: size = " << container.getSize() << endl;
         os << "[";
-        for (auto i = 0; i < container.getSize(); ++i){
-            // os << "(" << arr.m_data[i].GetValue() << ":" << arr.m_data[i].GetRef() << "),";
+        Node *pCurrent = container.m_pHat->GetNext();
+        while (pCurrent != container.m_pHat){
+            os << "(" << pCurrent->GetValue() << ":" << pCurrent->GetRef() << ")";
+            pCurrent = pCurrent->GetNext();
+            if(pCurrent != container.m_pHat) os << " , ";
+
         }
         os << "]" << endl;
         return os;
     }
+
     // TODO: Persistencia (read)
+    friend istream &operator>>(istream &is, CLinkedList<Traits> &container){
+        value_type val;
+        ref_type   ref;
+        char ch;
+        while (is >> ch){
+            if (ch == '('){
+                is >> val;
+                is >> ch; // :
+                is >> ref;
+                is >> ch; // )
+                container.push_back(val, ref);
+            }
+        }
+        return is;
+    }
+
 };
+
+//Constructor copia
+template <typename Traits>
+CLinkedList<Traits>::CLinkedList(const CLinkedList &other){
+    m_pHat = new Node();
+    m_pHat->GetNextRef() = m_pHat; // Lista circular
+    m_pLast = m_pHat;
+    m_nElements = 0;
+
+    Node *pCurrent = other.m_pHat->GetNext();
+    while (pCurrent != other.m_pHat){
+        value_type val = pCurrent->GetValue();
+        ref_type   ref = pCurrent->GetRef();
+        push_back(val, ref);
+        pCurrent = pCurrent->GetNext();
+    }
+}
+
+//Move Constructor
+template <typename Traits>
+CLinkedList<Traits>::CLinkedList(CLinkedList &&other){
+    m_pHat = other.m_pHat;
+    m_pLast = other.m_pLast;
+    m_nElements = other.m_nElements;
+
+    other.m_pHat = nullptr;
+    other.m_pLast = nullptr;
+    other.m_nElements = 0;
+}
+
+//Destructor virtual
+template <typename Traits>
+CLinkedList<Traits>::~CLinkedList(){
+    std::lock_guard<std::mutex> lock(m_mutex); // Bloquea el mutex durante la ejecucion de esta funcion
+    Node* curr = m_pHat->GetNext();
+    while (curr != m_pHat){
+        Node* tmp = curr;
+        curr = curr->GetNext();
+        delete tmp;
+    }
+    delete m_pHat;
+}
+
+//Operador de acceso [] sin const
+template <typename Traits>
+typename CLinkedList<Traits>::value_type &
+CLinkedList<Traits>::operator[](size_t index){
+    assert(index < m_nElements);
+
+    Node *pCurrent = m_pHat->GetNext();
+    for (size_t i = 0; i < index; ++i){
+        pCurrent = pCurrent->GetNext();
+    }
+    return pCurrent->GetValueRef();
+}
+
+
+
+//Operador de acceso con const []
+template <typename Traits>
+const typename CLinkedList<Traits>::value_type &CLinkedList<Traits>::operator[](size_t index) const {
+    assert (index < m_nElements);
+
+    Node *pCurrent = m_pHat->GetNext();
+    for (size_t i = 0; i < index; ++i){
+        pCurrent = pCurrent->GetNext();
+    }
+    return pCurrent->GetValueRef();
+}
+
+
 
 template <typename Traits>
 void CLinkedList<Traits>::push_back(value_type &val, ref_type ref){
-    Node *pNewNode = new Node(val, ref);
-    if( !m_pRoot )
-        m_pRoot = pNewNode;
-    m_pLast = pNewNode;
+    std::lock_guard<std::mutex> lock(m_mutex); 
+    Node *pNew = new Node(val, ref);
+    pNew->GetNextRef() = m_pHat; // Apunta al hat
+    m_pLast->GetNextRef() = pNew; // El ultimo apunta al nuevo
+    m_pLast = pNew; // Actualiza el ultimo
     ++m_nElements;
 }
 
 template <typename Traits>
+void CLinkedList<Traits>::pop_back(){
+    std::lock_guard<std::mutex> lock(m_mutex); 
+    assert(m_nElements > 0);
+
+    if(m_nElements == 1){
+        delete m_pLast;
+        m_pHat->GetNextRef() = m_pHat; // Lista vacía
+        m_pLast = m_pHat;
+    } else {
+        Node *prev = m_pHat;
+        while (prev->GetNext() != m_pLast){
+            prev = prev->GetNext();
+        }
+
+        prev->GetNextRef() = m_pHat; // El nuevo ultimo apunta al hat
+        delete m_pLast; // Elimina el antiguo ultimo
+        m_pLast = prev; // Actualiza el ultimo
+    }
+
+    --m_nElements;
+
+}
+
+//Como es lista circular, se usa Insert iterativo enves de un InternalInsert recursivo
+/*template <typename Traits>
 void CLinkedList<Traits>::InternalInsert(Node *&rParent, const value_type &val, ref_type ref){
     // TODO: Agregar algo para el caso de circular
     if( !rParent || rParent->m_data > val ){
@@ -116,11 +272,28 @@ void CLinkedList<Traits>::InternalInsert(Node *&rParent, const value_type &val, 
         return;
     }
     InternalInsert(rParent->GetNextRef(), val, ref);
-}
+}*/
 
 template <typename Traits>
 void CLinkedList<Traits>::Insert(const value_type &val, ref_type ref){
-    InternalInsert(m_pRoot, val, ref);
-}
+    std::lock_guard<std::mutex> lock(m_mutex); 
+    //InternalInsert(m_pHat, val, ref);
+    Node *pPrev = m_pHat;
+    Node *pCurrent = m_pHat->GetNext();
+    
+    while(pCurrent != m_pHat && pCurrent->GetValue() < val){
+        pPrev = pCurrent;
+        pCurrent = pCurrent->GetNext();
+    }
+
+    Node *pNew = new Node(val, ref);
+    pNew->GetNextRef() = pCurrent;
+    pPrev->GetNextRef() = pNew;
+
+    if( pCurrent == m_pHat) // Si se inserta al final, actualizar el ultimo
+        m_pLast = pNew;
+
+    ++m_nElements;
+} 
 
 #endif // __LINKEDLIST_H__
--- containers/stack.h
+++ containers/stack.h
@@ -1,13 +1,104 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
+#include <mutex>
+#include <cassert>
 #include <iostream>
 #include "../general/types.h"
 #include "../util.h"
+#include "../containers/linkedlist.h"
 
-class CStack{
-    
+using namespace std;
+
+template <typename T>
+struct StackTrait {
+    using value_type = T;
 };
+
+template <typename T>
+class CStack{
+    using Traits = StackTrait<T>;
+    using value_type = typename Traits::value_type;
+
+    private:
+    CLinkedList < AscendingTrait<value_type> > m_list;
+    mutable std::mutex m_mutex;
+
+    public:
+    CStack() {};
+
+    //Constructor copia
+    CStack(const CStack &other) {
+        std::lock_guard<std::mutex> lock(other.m_mutex);
+        m_list = other.m_list;
+    };
+
+    //Move Constructor
+    CStack(CStack &&other) {
+        std::lock_guard<std::mutex> lock(other.m_mutex);
+        m_list = std::move(other.m_list);
+    };
+
+    //Destructor seguro y virtual
+    virtual ~CStack() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        // El destructor de CLinkedList se encargará de liberar la memoria
+    };
+
+    // Método para imprimir el stack (para operator<<)
+    value_type top() const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        assert(m_list.getSize() > 0);
+        return m_list.get_front(); // Retorna el valor del tope (último elemento) 
+    };
+
+    //Push (LIFO)
+    void push(const value_type &value) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_list.push_front(const_cast<value_type&>(value)); // Agrega el nuevo valor al frente de la lista (tope del stack)
+    };
+
+    //Pop (LIFO)
+    value_type pop() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        assert(m_list.getSize() > 0);
+
+        value_type value = m_list.get_front(); // Obtener el valor del tope
+        m_list.pop_front(); // Eliminar el primer elemento (tope del stack)
+        return value; // Retornar el valor del tope
+    };
+
+    // Método para imprimir el stack (para operator<<)
+    void print(std::ostream &os = std::cout) const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        os << "[top] ";
+        if(m_list.getSize() > 0) {
+            os << m_list.get_front(); // Imprime el valor del tope
+        } else {
+            os << "(stack vacío)";
+        }
+    };
+
+    //operator<<
+    CStack& operator<<(const value_type& value) {
+        push(value);
+        return *this;
+    };
+
+    //operator>>
+    CStack& operator>>(value_type& value) {
+        value = top();
+        pop();
+        return *this;
+    };
+};
+
+template <typename T>
+ostream& operator<<(std::ostream& os, const CStack<T>& stack) {
+    stack.print(os);
+    return os;
+}
+
 
 
 #endif // __STACK_H__
