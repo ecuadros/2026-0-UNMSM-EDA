5f5088cb9b5a2fafaee4f139e9fc271abf91a963
Denis Tintaya <denistintaya@unmsm.com>
2026-02-11 23:25:53 -0500

    CStack

--- Demos.cpp
+++ Demos.cpp
@@ -1,13 +1,75 @@
 #include <iostream>
-#include "containers/lists.h"
+#include "containers/stack.h"
+
 
 using namespace std;
+using T1 = int;
 
-void DemoLists(){
-    CLinkedList< AscendingTrait<T1> > l1;
+void DemoStack(){
 
-    l1.Insert(20, 5);
-    l1.Insert(30, 3);
-    cout << l1 << endl;
+    // Push()
+ 
+    CStack< StackTrait<T1, std::less<T1>> > s1;
+
+    s1.push(1);
+    s1.push(-1);
+    s1.push(5);
+    s1.push(8);
+    s1.push(78);
+
+    cout << s1 << endl << endl;
+
+    // Top()
+
+    cout << "Elemento en el top: " << s1.top() << endl << endl;
+
+    // Pop()
+
+    s1.pop();
+    cout << "Tras pop():  ";
+    cout << s1 << endl;
+    cout << "Nuevo top: " << s1.top() << endl << endl;
+
+    // getSize() y empty()
+    
+    cout << "Size = " << s1.getSize() << endl;
+    cout << "¿Está vacío? " << (s1.empty() ? "Sí" : "No") << endl << endl;
+
+    // Copy constructor 
+
+    CStack< StackTrait<T1, std::less<T1>> > s2 = s1;
+
+    cout << "s1:  " << s1 << endl;
+    cout << "s2 (copia):  " << s2 << endl;
+
+    s2.pop();
+
+    cout << "s1 (intacto):  " << s1 << endl;
+    cout << "s2 (modificado):  " << s2 << endl << endl;
+
+    // Move constructor
+    
+    CStack< StackTrait<T1, std::less<T1>> > s3 = std::move(s1);
+
+    cout << "s1 tras move:  " << s1 << endl;
+    cout << "s3 (recursos de s1):  " << s3 << endl << endl;
+
+
+    // Clear()
+  
+    s3.clear();
+
+    cout << "s3 tras clear:  " << s3 << endl;
+    cout << "¿Está vacío? " << (s3.empty() ? "Sí" : "No") << endl << endl;
+
+    // Operator>>
+   
+    CStack< StackTrait<T1, std::less<T1>> > s5;
+
+    cin >> s5;
+
+    cout << "Stack ingresado:  ";
+    cout << s5 << endl << endl;
+
 }
 
--- containers/stack.h
+++ containers/stack.h
@@ -2,12 +2,190 @@
 #define __STACK_H__
 
 #include <iostream>
-#include "../general/types.h"
-#include "../util.h"
+#include <mutex>
+#include <cassert>
+#include <utility>
 
-class CStack{
-    
+
+template <typename T, typename _Func>
+struct StackTrait{
+    using value_type = T;
+    using Func       = _Func;
 };
 
+
+template <typename Traits>
+class NodeStack{
+
+    using value_type = typename Traits::value_type;
+    using Node       = NodeStack<Traits>;
+
+private:
+    value_type m_data;
+    Node      *m_pNext = nullptr;
+
+public:
+    NodeStack(){}
+    NodeStack(value_type val) : m_data(val){}
+
+    value_type  GetValue() const { return m_data; }
+    value_type &GetValueRef()    { return m_data; }
+
+    Node*  GetNext() const { return m_pNext; }
+    Node*& GetNextRef()    { return m_pNext; }
+};
+
+
+
+template <typename Traits>
+class CStack{
+
+public:
+    using value_type = typename Traits::value_type;
+    using Node       = NodeStack<Traits>;
+
+private:
+    Node     *m_pTop = nullptr;
+    size_t    m_nElements = 0;
+    mutable   std::mutex m_mutex;
+
+public:
+    CStack(){}
+
+    // Copy Constructor
+
+    CStack(const CStack &another){
+        std::lock_guard<std::mutex> lock(another.m_mutex);
+
+        assert( another.m_pTop != nullptr );
+        Node *tem = another.m_pTop;
+        Node *prev = nullptr;
+
+        while (temp){
+            Node *pNew = new Node(temp->GetValue());
+            pNew->GetNextRef() = prev;
+            prev = pNew;
+            temp = temp->GetNext();
+        }
+
+        m_pTop = prev;
+        m_nElements = another.m_nElements;
+    }
+
+
+    // Move Constructor
+
+    CStack(CStack &&another) noexcept{
+        std::lock_guard<std::mutex> lock(another.m_mutex);
+
+        m_pTop      = std::exchange(another.m_pTop, nullptr);
+        m_nElements = std::exchange(another.m_nElements, 0);
+    }
+
+    // Destructor
+
+    virtual ~CStack(){
+        clear();
+    }
+
+    void push(const value_type &val){
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        Node *pNew = new Node(val);
+        pNew->GetNextRef() = m_pTop;
+        m_pTop = pNew;
+
+        ++m_nElements;
+    }
+
+    void pop(){
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        assert(m_pTop != nullptr);
+
+        Node *pDelete = m_pTop;
+        m_pTop = m_pTop->GetNext();
+
+        delete pDelete;
+        --m_nElements;
+    }
+
+    value_type &top(){
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_pTop->GetValueRef();
+    }
+
+    const value_type &top() const{
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_pTop->GetValue();
+    }
+
+    bool empty() const{
+        return m_nElements == 0;
+    }
+
+    Size getSize() const{
+        return m_nElements;
+    }
+
+    void clear() noexcept{
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        Node *temp = m_pTop;
+        while (temp){
+            Node *n = temp->GetNext();
+            delete temp;
+            temp = n;
+        }
+
+        m_pTop = nullptr;
+        m_nElements = 0;
+    }
+
+    // Operator<<
+    
+
+    friend std::ostream &operator<<(std::ostream &os, const CStack &stack){
+        std::lock_guard<std::mutex> lock(stack.m_mutex);
+
+        os << "CStack: size = " << stack.m_nElements << std::endl;
+        os << "Top -> ";
+
+        Node *temp = stack.m_pTop;
+        while (temp){
+            os << temp->GetValue();
+            if (temp->GetNext()) 
+            os << " -> ";
+            temp = temp->GetNext();
+        }
+        
+        return os;
+    }
+
+    // Operator>>
+
+    friend std::istream &operator>>(std::istream &is, CStack &stack){
+        std::lock_guard<std::mutex> lock(stack.m_mutex);
+
+        stack.clear();
+
+        cout << "¿Cuántos elementos desea añadir al stack? ";
+        size_t count;
+        is >> count;
+
+        for (size_t i = 0; i < count; ++i){
+            value_type val;
+
+            cout << "Elemento " << (i + 1) << ": ";
+            is >> val;
+
+            stack.push(val);
+        }
+
+        return is;
+    }
+};
+
+void DemoStack();
 
 #endif // __STACK_H__
