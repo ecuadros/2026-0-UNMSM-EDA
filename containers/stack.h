4dfe1bea42535181e65f85555f312b791e23e1c1
Zapata Llaxa Jorge Andres <zap123.joan.re@gmail.com>
2026-02-12 21:25:28 -0500

    Implement thread-safe CStack class
    Added a thread-safe stack implementation with a mutex for concurrency control.

--- containers/stack.h
+++ containers/stack.h
@@ -1,13 +1,29 @@
 #ifndef __STACK_H__
 #define __STACK_H__
-
 #include <iostream>
+#include <mutex>
+#include <stdexcept>
 #include "../general/types.h"
 #include "../util.h"
+using namespace std;
 
-class CStack{
-    
-};
+// Stack (LIFO)
+template <typename T>
+class CStack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& val) : data(val), next(nullptr) {}
+    };
+
+    Node* m_pTop;
+    size_t m_nElements;
+    mutable std::mutex m_mutex; // Concurrencia
+
+public:
+    // Constructor
+    CStack() : m_pTop(nullptr), m_nElements(0) {}
 
 
 #endif // __STACK_H__
