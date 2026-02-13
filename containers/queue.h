3fe67615a2220efabf4ec4995cdf752ffd196ebd
Zapata Llaxa Jorge Andres <zap123.joan.re@gmail.com>
2026-02-12 22:07:54 -0500

    Implement basic CQueue class with thread safety

--- containers/queue.h
+++ containers/queue.h
@@ -1,13 +1,30 @@
 #ifndef __QUEUE_H__
 #define __QUEUE_H__
-
 #include <iostream>
+#include <mutex>
+#include <stdexcept>
 #include "../general/types.h"
 #include "../util.h"
+using namespace std;
 
-class CQueue{
-    
-};
+// Queue (FIFO)
+template <typename T>
+class CQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& val) : data(val), next(nullptr) {}
+    };
+
+    Node* m_pFront;
+    Node* m_pRear;
+    size_t m_nElements;
+    mutable std::mutex m_mutex; // Concurrencia
+
+public:
+    // Constructor
+    CQueue() : m_pFront(nullptr), m_pRear(nullptr), m_nElements(0) {}
 
 
 #endif // __QUEUE_H__
