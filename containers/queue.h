3b8718beb7101bd5a02427f92f751b67ba83673d
Zapata Llaxa Jorge Andres <zap123.joan.re@gmail.com>
2026-02-12 22:08:14 -0500

    Update queue.h

--- containers/queue.h
+++ containers/queue.h
@@ -26,5 +26,60 @@ public:
     // Constructor
     CQueue() : m_pFront(nullptr), m_pRear(nullptr), m_nElements(0) {}
 
+    // Constructor copia
+    CQueue(const CQueue& other) : m_pFront(nullptr), m_pRear(nullptr), m_nElements(0) {
+        std::lock_guard<std::mutex> lock(other.m_mutex);
+        
+        Node* curr = other.m_pFront;
+        while (curr) {
+            push(curr->data);
+            curr = curr->next;
+        }
+    }
+
+    // Move Constructor
+    CQueue(CQueue&& other) noexcept
+        : m_pFront(other.m_pFront), m_pRear(other.m_pRear), m_nElements(other.m_nElements) {
+        other.m_pFront = nullptr;
+        other.m_pRear = nullptr;
+        other.m_nElements = 0;
+    }
+
+    // Destructor seguro
+    virtual ~CQueue() {
+        clear();
+    }
+
+    CQueue& operator=(const CQueue& other) {
+        if (this != &other) {
+            std::lock(m_mutex, other.m_mutex);
+            std::lock_guard<std::mutex> lock1(m_mutex, std::adopt_lock);
+            std::lock_guard<std::mutex> lock2(other.m_mutex, std::adopt_lock);
+            
+            clear();
+            
+            Node* curr = other.m_pFront;
+            while (curr) {
+                push(curr->data);
+                curr = curr->next;
+            }
+        }
+        return *this;
+    }
+
+    CQueue& operator=(CQueue&& other) noexcept {
+        if (this != &other) {
+            clear();
+            m_pFront = other.m_pFront;
+            m_pRear = other.m_pRear;
+            m_nElements = other.m_nElements;
+            
+            other.m_pFront = nullptr;
+            other.m_pRear = nullptr;
+            other.m_nElements = 0;
+        }
+        return *this;
+    }
+
 
 #endif // __QUEUE_H__
