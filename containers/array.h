ba8ec45a01e3e0a252cef5981c9f193e4e86265d
Gianmarco Blas <gianmarcoblassanca@gmail.com>
2026-02-19 00:52:49 -0500

    Cambios en el array

--- containers/array.h
+++ containers/array.h
@@ -7,6 +7,8 @@ using namespace std;
 #include "../algorithms/sorting.h"
 #include "GeneralIterator.h"
 
+using namespace std;
+
 template <typename _T>
 struct Trait1
 {
@@ -16,13 +18,13 @@ struct Trait1
 template <typename Container>
 class ArrayForwardIterator : public GeneralIterator<Container>
 { 
-  using Parent = GeneralIterator<Container>;
-  public:
-    ArrayForwardIterator(Container *pContainer, Size pos=0)       : Parent(pContainer, pos){}
-    ArrayForwardIterator(ArrayForwardIterator<Container> &another):  Parent(another){}
+    using Parent = GeneralIterator<Container>;
+public:
+    ArrayForwardIterator(Container *pContainer, size_t pos = 0) : Parent(pContainer, pos) {}
+    ArrayForwardIterator(const ArrayForwardIterator<Container> &another) : Parent(another) {}
 
-    ArrayForwardIterator<Container> &operator++(){
-        if( Parent::m_pos < Parent::m_pContainer->getSize() )
+    ArrayForwardIterator<Container> &operator++() {
+        if (Parent::m_pos < Parent::m_pContainer->size())
             ++Parent::m_pos;
         return *this;
     }
@@ -51,40 +53,17 @@ class CArray {
     using  backward_iterator = ArrayBackwardIterator< CArray<Traits> >;
     friend backward_iterator;
     friend GeneralIterator< CArray<Traits> >;
-
-    struct Node{
-        value_type m_value;
-        ref_type   m_ref;
-
-        Node(){}
-        Node( value_type _value, ref_type _ref = -1)
-            : m_value(_value), m_ref(_ref){   }
-        value_type  GetValue   () const { return m_value; }
-        value_type &GetValueRef() { return m_value; }
-
-        ref_type    GetRef     () const { return m_ref;   }
-        ref_type   &GetRefRef  () { return m_ref;   }
-        Node &operator=(const Node &another){
-          m_value = another.GetValue();
-          m_ref   = another.GetRef();
-          return *this;
-        }
-        bool operator==(const Node &another) const
-        { return m_value == another.GetValue();   }
-        bool operator<(const Node &another) const
-        { return m_value < another.GetValue();   }
-    };
-    //using  CompareFunc = Traits::CompareFunc
-    using  CompareFunc = bool (*)(const Node &, const Node &);
   private:
     Size m_capacity = 0, m_last = 0;
-    Node *m_data = nullptr;
+    value_type *m_data = nullptr;
 
   public:
-    CArray(Size size);
+    CArray(size_t Capacity=10);
     virtual ~CArray();
 
-    void push_back(value_type value, ref_type ref);
+    void push_back(const value_type &value);
+    void pop_back();
+    value_type &back();
     value_type &operator[](Size index);
     Size getSize() const
     {   return m_last + 1;  };
@@ -113,67 +92,70 @@ class CArray {
     }
     friend ostream &operator<<(ostream &os, CArray<Traits> &container){
         os << "CArray: size = " << container.getSize() << endl;
-        os << "[";
-        for (auto i = 0; i < container.getSize(); ++i)
-          os << "(" << container.m_data[i].GetValue() << ":" << container.m_data[i].GetRef() << "),";
+        os << "[ ";
+        if (container.m_last != (size_t)-1) {
+            for (size_t i = 0; i <= container.m_last; ++i)
+              os << container.m_data[i] << " ";
+        }
         os << "]" << endl;
         return os;
     }
 };
 
 template <typename Traits>
-CArray<Traits>::CArray(Size size) {
-  m_capacity = size;
-  m_data = new Node[size];
+CArray<Traits>::CArray(size_t capacity) {
+    if (capacity == 0) capacity = 10;
+    m_capacity = capacity;
+   m_last = 0;
+    m_data = new value_type[capacity];
 }
 template <typename Traits>
 CArray<Traits>::~CArray() { delete[] m_data; }
 
 template <typename Traits>
 typename CArray<Traits>::value_type &CArray<Traits>::operator[](Size index) {
-    // cout << "XResizing from " << m_capacity << " to at least " << index + 5 << endl;
-    if (index > m_capacity) {
-      cout << "Resizing from " << m_capacity << " to at least " << index + 5 << endl;
-      resize(index - m_last + 5);
+    if (index >= m_capacity) {
+        resize(index - m_last + 5);
     }
     assert(index < m_capacity);
-    if (index > m_last)
-      m_last = index;
-    return m_data[index].GetValueRef();
+    if (index >= m_capacity) {
+        m_last = index ;
+    }
+    return m_data[index];
 }
 
 template <typename Traits>
-void CArray<Traits>::push_back(value_type value, ref_type ref) {
+void CArray<Traits>::push_back(const value_type &value) {
     if (m_last >= m_capacity)
-      resize();
-    m_data[m_last++] = Node(value, ref);
+      resize(m_capacity); 
+    m_data[m_last++] = value;
+}
+
+template <typename Traits>
+void CArray<Traits>::pop_back() {
+    if (m_last > 0)
+        m_last--;
+}
+
+template <typename Traits>
+typename CArray<Traits>::value_type &CArray<Traits>::back() {
+    assert(m_last > 0);
+    return m_data[m_last - 1];
 }
 
 template <typename Traits>
 void CArray<Traits>::resize(Size delta) {
-    Size new_capacity = m_capacity + delta;
-    Node *new_data = new Node[new_capacity];
-    for (auto i = 0; i < m_capacity; ++i)
+    size_t new_capacity = m_capacity + delta;
+    value_type *new_data = new value_type[new_capacity];
+    for (size_t i = 0; i < m_capacity; ++i)
       new_data[i] = m_data[i];
     delete[] m_data;
     m_data = new_data;
     m_capacity = new_capacity;
 }
 
-template <typename Traits>
-void CArray<Traits>::sort( CompareFunc pComp ){
-    BurbujaRecursivo(m_data, m_last+1, pComp);
-}
 
-// template <typename Traits>
-// ostream &operator<<(ostream &os, CArray<Traits> &arr) {
-//   os << "CArray: size = " << arr.getSize() << endl;
-//   os << "[";
-//   for (auto i = 0; i < arr.getSize(); ++i)
-//     os << "(" << arr.m_data[i].GetValue() << ":" << arr.m_data[i].GetRef() << "),";
-//   os << "]" << endl;
-//   return os;
-// }
+
 
 void DemoArray();
 
--- containers/heap.h
+++ containers/heap.h
@@ -4,11 +4,58 @@
 #include <iostream>
 #include "../general/types.h"
 #include "../util.h"
-
-class CHeap{
-    // ...
-    
+template <typename T, typename _Func>
+struct ListTrait{
+    using value_type = T;
+    using Func       = _Func;
 };
+
+template <typename T>
+struct MinHeap : 
+    public ListTrait<T, std::greater<T> >{
+};
+
+template <typename T>
+struct MaxHeap : 
+    public ListTrait<T, std::less<T> >{
+};
+
+template <typename Trait>
+class CHeap{
+    public:
+    using value_type=typename Traits::value_type;
+    private:
+
+    public:
+    CHeap(){};
+    //COPY CONSTRUCTOR
+    CHeap(const CHeap &another){
+
+    }
+    //MOVE CONSTRUCTOR
+     CHeap(CHeap &&another) noexcept{
+
+     }
+    //DESTRUCTOR
+    virtual ~CHeap(){};
+    value_type  Pop();
+    void   Push(value_type &Val);
+};
+template <typename Traits>
+void  CHeap<Traits>::Push(value_type &val){
+
+}
+template <typename Traits>
+typename  CHeap<Traits>::value_type CHeap<Traits>::Pop(){
+
+}
+
+
+template <typename Traits>
+CHeap<Traits>::~CHeap() {
+    
+}
+
 
 
 #endif // __HEAP_H__
