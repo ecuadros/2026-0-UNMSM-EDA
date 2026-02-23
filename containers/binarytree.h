7c48bd5fd05bc957e71012d9d02fa53dc06781cb
Aaron <aaron.rojas@unmsm.edu.pe>
2026-02-23 13:05:06 -0500

    El arbol binario (B-Tree) ya esta implementado en containers/binarytree.h

--- containers/binarytree.h
+++ containers/binarytree.h
@@ -4,7 +4,8 @@
 #include <iostream>
 #include "../general/types.h"
 #include "../util.h"
-
+#include <mutex>
+#include <vector>
 
 template <typename T>
 struct TreeTraitAscending {
@@ -20,44 +21,175 @@ struct TreeTraitDescending {
 
 template <typename Traits>
 class NodeBinaryTree{
+    // Permitir que CBinaryTree acceda a los miembros privados de NodeBinaryTree
+    template <typename T>
+    friend class CBinaryTree;
+
     using  value_type  = typename Traits::value_type;
-    using  Node        = typename NodeBinaryTree<Traits>;
+    using  Node        = NodeBinaryTree<Traits>;
     using  CompareFunc = typename Traits::CompareFunc;
+
 private:
     value_type m_data;
     ref_type   m_ref;
-    Node *m_pChild[2]  = nullptr;
+    Node *m_pChild[2]  = {nullptr, nullptr};
 public:
     NodeBinaryTree(){}
     NodeBinaryTree( value_type _value, ref_type _ref = -1)
         : m_data(_value), m_ref(_ref){   }
     value_type  GetValue   () const { return m_data; }
     value_type &GetValueRef() { return m_data; }
+
+    // Agregar un campo para almacenar la referencia
+    ref_type GetRef() const { return m_ref; }
 };
 
 template <typename Traits>
 class CBinaryTree{
 public:
     using  value_type  = typename Traits::value_type;
-    using  Node        = typename NodeBinaryTree<Traits>;
+    using  Node        = NodeBinaryTree<Traits>;
     using  CompareFunc = typename Traits::CompareFunc;
+
 private:
     Node *m_pRoot = nullptr;
     CompareFunc comp;
 
+    std::mutex mtx; // Mutex para operaciones concurrentes
+
+    mutable std::vector<value_type> m_cache; // Cache para iteradores
 public:
     CBinaryTree(){}
-    // TODO: Copy constructor
-    CBinaryTree(CBinaryTree &another){
 
+    // TODO: Copy constructor
+    CBinaryTree(const CBinaryTree &another){
+        m_pRoot = Copy(another.m_pRoot);
+        comp = another.comp;
     }
+
     // TODO: Move constructor
     CBinaryTree(CBinaryTree &&another){
-
+        m_pRoot = another.m_pRoot;
+        comp = another.comp;
+        another.m_pRoot = nullptr;
     }
-    virtual ~CBinaryTree(){
 
+    //Destructor
+    ~CBinaryTree(){
+        clear(m_pRoot);
     }
+
+    void Remove(const value_type &val){
+        std::lock_guard<std::mutex> lock(mtx); // Bloquea el mutex durante la operación
+        m_pRoot = InternalRemove(m_pRoot, val);
+    }
+
+    //Inorder variadic
+    template <typename... Func>
+    void InOrder(Func... funcs){
+        InOrder(m_pRoot, funcs...);
+    }
+
+    //Preorder variadic
+    template <typename... Func>
+    void PreOrder(Func... funcs){
+        PreOrder(m_pRoot, funcs...);
+    }
+
+    //Postorder variadic
+    template <typename... Func>
+    void PostOrder(Func... funcs){
+        PostOrder(m_pRoot, funcs...);
+    }
+
+    //ForEach
+    template <typename... Func>
+    void ForEach(Func... funcs) {
+        InOrder(funcs...);
+    }
+
+    //FirstThat
+    template <typename Predicate>
+    value_type* FirstThat(Predicate pred) {
+        return FirstThatInternal(m_pRoot, pred);
+    }
+
+    //Operador de acceso <<
+    bool operator<<(const CBinaryTree &other)const{
+        if(!m_pRoot || !other.m_pRoot){
+            return false; // No se puede comparar si alguno de los árboles está vacío
+        }
+
+        return m_pRoot->GetValue() == other.m_pRoot->GetValue();
+    }
+
+    //Operador de acceso >>
+    bool operator>>(const CBinaryTree &other)const{
+        if(!m_pRoot || !other.m_pRoot){
+            return false; // No se puede comparar si alguno de los árboles está vacío
+        }
+
+        return m_pRoot->GetValue() > other.m_pRoot->GetValue();
+    }
+
+    // Método para convertir el árbol a un vector (in-order)
+    std::vector<value_type> ToVector() const{
+        std::vector<value_type> result;
+        ToVectorInternal(m_pRoot, result);
+        return result;
+    }
+
+    void ToVectorInternal(Node* node, std::vector<value_type>& vec) const {
+        if (!node) {
+            return;
+        }
+        ToVectorInternal(node->m_pChild[0], vec);
+        vec.push_back(node->GetValue());
+        ToVectorInternal(node->m_pChild[1], vec);
+    }
+
+    // Iteradores
+    auto begin(){
+        m_cache = ToVector(); // Cache del árbol en un vector
+        return m_cache.begin();
+    }
+
+    auto end(){
+        return m_cache.end();
+    }
+
+    auto rbegin(){
+        m_cache = ToVector(); // Cache del árbol en un vector
+        return m_cache.rbegin();
+    }
+
+    auto rend(){
+        return m_cache.rend();
+    }
+
+    // Rotaciones
+    Node* RotateRight(Node* y) {
+        Node* x = y->m_pChild[0];
+        Node* T2 = x->m_pChild[1];
+
+        // Realizar rotación
+        x->m_pChild[1] = y;
+        y->m_pChild[0] = T2;
+
+        return x; // Nueva raíz después de la rotación
+    }
+
+    Node* RotateLeft(Node* x) {
+        Node* y = x->m_pChild[1];
+        Node* T2 = y->m_pChild[0];
+
+        // Realizar rotación
+        y->m_pChild[0] = x;
+        x->m_pChild[1] = T2;
+
+        return y; // Nueva raíz después de la rotación
+    }
+
 private:
     void InternalInsert(Node *&rParent, const value_type &val, ref_type ref){
         if( !rParent ){
@@ -69,7 +201,132 @@ private:
     }
 public:
     void Insert(const value_type &val, ref_type ref){
+        std::lock_guard<std::mutex> lock(mtx); // Bloquea el mutex durante la operación
         InternalInsert(m_pRoot, val, ref);
+    }
+
+//Lo nuevo que se esta implementando
+private:
+    void clear(Node* node){
+        if (!node) {
+            return;
+        }
+        clear(node->m_pChild[0]);
+        clear(node->m_pChild[1]);
+        delete node;
+    }
+
+    Node* Copy(Node* node) {
+        if (!node) {
+            return nullptr;
+        }
+        Node* newNode = new Node(node->GetValue(), node->GetRef());
+        newNode->m_pChild[0] = Copy(node->m_pChild[0]);
+        newNode->m_pChild[1] = Copy(node->m_pChild[1]);
+        return newNode;
+    }
+
+    Node* FindMin(Node* node) {
+        while (node && node->m_pChild[0]) {
+            node = node->m_pChild[0];
+        }
+        return node;
+    }
+
+    Node* InternalRemove(Node* node, const value_type& val) {
+        if (!node) return nullptr;
+        auto path = comp(val, node->GetValue());
+
+        if (val == node->GetValue()) {
+
+            // Caso 1: sin hijo izquierdo
+            if (!node->m_pChild[0]) {
+                Node* temp = node->m_pChild[1];
+                delete node;
+                return temp;
+            }
+
+            // Caso 2: sin hijo derecho
+            if (!node->m_pChild[1]) {
+                Node* temp = node->m_pChild[0];
+                delete node;
+                return temp;
+            }
+
+            // Caso 3: dos hijos
+            Node* successor = node->m_pChild[1];
+
+            while (successor->m_pChild[0]) {
+                successor = successor->m_pChild[0];
+            }
+
+            node->m_data = successor->m_data;
+            node->m_ref  = successor->m_ref;
+
+            node->m_pChild[1] =
+                InternalRemove(node->m_pChild[1], successor->GetValue());
+        }
+        else {
+            node->m_pChild[path] =
+                InternalRemove(node->m_pChild[path], val);
+        }
+
+        return node;
+    }
+
+    //Inorder variadic
+    template <typename... Func>
+    void InOrder(Node* node, Func... funcs) {
+        if (!node) {
+            return;
+        }
+        
+        InOrder(node->m_pChild[0], funcs...);
+        (funcs(node->GetValue()), ...); // Llama a cada función con el valor del nodo
+        InOrder(node->m_pChild[1], funcs...);
+    }
+
+    //Preorder variadic
+    template <typename... Func>
+    void PreOrder(Node* node, Func... funcs) {
+        if (!node) {
+            return;
+        }
+        
+        (funcs(node->GetValue()), ...); // Llama a cada función con el valor del nodo
+        PreOrder(node->m_pChild[0], funcs...);
+        PreOrder(node->m_pChild[1], funcs...);
+    }
+
+    //Postorder variadic
+    template <typename... Func>
+    void PostOrder(Node* node, Func... funcs) {
+        if (!node) {
+            return;
+        }
+        
+        PostOrder(node->m_pChild[0], funcs...);
+        PostOrder(node->m_pChild[1], funcs...);
+        (funcs(node->GetValue()), ...); // Llama a cada función con el valor del nodo
+    }
+
+    //FirstThat Internal
+    template <typename Predicate>
+    value_type* FirstThatInternal(Node* node, Predicate pred) {
+        if (!node) {
+            return nullptr;
+        }
+        
+        auto left = FirstThatInternal(node->m_pChild[0], pred);
+        if (left) {
+            return left;
+        }
+
+        if (pred(node->GetValue())) {
+            return &(node->GetValueRef());
+        }
+
+        return FirstThatInternal(node->m_pChild[1], pred);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include "util.h"
 #include "pointers.h"
 #include "containers/array.h"
@@ -12,5 +12,98 @@ int main() {
     // DemoPointers1();
     // DemoArray();
     DemoLists();
+    return 0;
+}*/
+
+#include <iostream>
+#include "containers/binarytree.h"
+
+using namespace std;
+
+int main(){
+
+    // Crear árbol ascendente (BST normal)
+    CBinaryTree<TreeTraitAscending<int>> tree;
+
+    cout << "Insertando elementos..." << endl;
+
+    tree.Insert(50, 1);
+    tree.Insert(30, 2);
+    tree.Insert(70, 3);
+    tree.Insert(20, 4);
+    tree.Insert(40, 5);
+    tree.Insert(60, 6);
+    tree.Insert(80, 7);
+
+    cout << "\nRecorrido InOrder: ";
+    tree.InOrder([](int v){ cout << v << " "; });
+
+    cout << "\nRecorrido PreOrder: ";
+    tree.PreOrder([](int v){ cout << v << " "; });
+
+    cout << "\nRecorrido PostOrder: ";
+    tree.PostOrder([](int v){ cout << v << " "; });
+
+    // Probar FirstThat
+    auto ptr = tree.FirstThat([](int v){ return v > 55; });
+    if(ptr)
+        cout << "\n\nPrimer valor mayor que 55: " << *ptr << endl;
+
+    // Probar iteradores
+    cout << "\nIterador forward: ";
+    for(auto it = tree.begin(); it != tree.end(); ++it){
+        cout << *it << " ";
+    }
+
+    cout << "\nIterador reverse: ";
+    for(auto it = tree.rbegin(); it != tree.rend(); ++it){
+        cout << *it << " ";
+    }
+
+    cout << "\nAntes de eliminar:\n";
+    tree.InOrder([](int v){ cout << v << " "; });
+
+    tree.Remove(70);
+
+    cout << "\nDespues de eliminar:\n";
+    tree.InOrder([](int v){ cout << v << " "; });
+    
+    // Probar Remove
+    cout << "\n\nEliminando 70..." << endl;
+    tree.Remove(70);
+
+    cout << "InOrder despues de eliminar: ";
+    tree.InOrder([](int v){ cout << v << " "; });
+
+    // Probar constructor copia
+    cout << "\n\nProbando constructor copia..." << endl;
+    CBinaryTree<TreeTraitAscending<int>> treeCopy(tree);
+
+    cout << "Arbol copia InOrder: ";
+    treeCopy.InOrder([](int v){ cout << v << " "; });
+
+    // Probar move constructor
+    cout << "\n\nProbando move constructor..." << endl;
+    CBinaryTree<TreeTraitAscending<int>> treeMove(std::move(treeCopy));
+
+    cout << "Arbol movido InOrder: ";
+    treeMove.InOrder([](int v){ cout << v << " "; });
+
+    // Probar operadores
+    cout << "\n\nProbando operadores..." << endl;
+
+    CBinaryTree<TreeTraitAscending<int>> tree2;
+    tree2.Insert(50, 1);
+
+    if(treeMove << tree2)
+        cout << "Las raices son iguales (operator<<)" << endl;
+
+    if(treeMove >> tree2)
+        cout << "Raiz del primero es mayor (operator>>)" << endl;
+    else
+        cout << "Raiz del primero NO es mayor" << endl;
+
+    cout << "\nPrograma finalizado correctamente." << endl;
+
     return 0;
 }
