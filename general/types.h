d73bd0e35b338bf60e20e384c45645955ddde15b
OneMore24 <alanjesus1946@outlook.com>
2026-02-25 22:16:34 -0500

    Modified code for BTree

--- general/types.h
+++ general/types.h
@@ -1,7 +1,10 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
+#include <string>
 
 using T1 = int;
+using T2 = std::string;
+using LSize = long;
 using T  = unsigned int;
 using TT = unsigned long long;
 using Size = int;
